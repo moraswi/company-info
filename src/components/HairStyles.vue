402e9eb1b8a2797761fe0839b23042a432c747f8
Phineas Lekgothoane <phineas@softidoc.co.za>
2022-10-15 08:25:20 +0200

    json folder

--- src/components/HairStyles.vue
+++ src/components/HairStyles.vue
@@ -2,15 +2,12 @@
   <v-container class="">
     <v-card class="transparent" height="160" flat tile></v-card>
     <v-card class="transparent" align="center" flat tile>
-      <v-card to="/location" class="mt-10" width="50%" height="130px">
-        <h1>Product Name</h1>
-        <h2>Price: R120</h2>
-      </v-card>
-
-      <v-card class="mt-10" width="50%" height="130px">
-        <h1>Product Name</h1>
-        <h2>Price: R120</h2>
-      </v-card>
+      <div v-for="job in jobs" :key="job.id" class="job">
+        <v-card class="mt-10" width="50%" height="130px">
+          <h1>{{ job.id }} {{ job.title }}</h1>
+          <p>{{ job.details }}</p>
+        </v-card>
+      </div>
     </v-card>
   </v-container>
 </template>
@@ -18,7 +15,14 @@
 <script>
 export default {
   name: "HairStyles",
-  data: () => ({}),
+  data: () => ({ jobs: [] }),
+
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((error) => console.log(error.message));
+  },
 };
 </script>
 
